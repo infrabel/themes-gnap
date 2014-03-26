034ee242664f968b39d02cd1ff5d192af8093ac1
Infrabel <dotnet@infrabel.be>
2014-03-26 22:00:28 +0100

    Added default skeleton

---
+++ tasks/default.js
@@ -0,0 +1,6 @@
+﻿module.exports = function(grunt) {
+    grunt.registerTask('default',
+    [
+        'clean'
+    ]);
+};
---
+++ tasks/util.js
@@ -0,0 +1,16 @@
+﻿module.exports = function(grunt) {
+    grunt.registerTask('task-wildcard-target', 'Queues all tasks of a given type which targets which match the prefix', function (task, targetPrefix) {
+        if (!task)
+            return;
+
+        if (!targetPrefix)
+            return;
+
+        var definedReplaces = Object.keys(grunt.config.data[task]);
+
+        definedReplaces.forEach(function (definedReplace) {
+            if (definedReplace.startsWith(targetPrefix))
+                grunt.task.run(task + ':' + definedReplace);
+        });
+    });
+};
