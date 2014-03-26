3efb62a5a568f3c6ffc1ba5b8a03b080db88d6df
David Cumps <david@cumps.be>
2014-03-26 22:00:28 +0100

    Added default skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+﻿if (typeof String.prototype.endsWith != 'function') {
+    String.prototype.endsWith = function (str) {
+        return this.slice(-str.length) == str;
+    };
+}
+
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str) {
+        return this.slice(0, str.length) == str;
+    };
+}
+
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.util.linefeed = '\r\n';
+
+    var config = { 
+        pkg: grunt.file.readJSON('package.json'),
+    };
+
+    merge(config, loadConfig('./tasks/options/', '.js'));
+
+    //console.log(config);
+
+    grunt.initConfig(config);
+    
+    require('load-grunt-tasks')(grunt);
+
+    grunt.loadTasks('tasks');
+};
+
+function loadConfig(path, extension) {
+    var glob = require('glob'),
+        object = {},
+        key;
+
+    glob.sync('**/*' + extension, { cwd: path }).forEach(function (option) {
+        key = option.replace(extension, '');
+        key = key.split('.').slice(-1);
+        
+        if (key in object) {
+            object[key] = merge(object[key], require(path + option));
+        } else {
+            object[key] = require(path + option);
+        }
+    });
+
+    return object;
+}
+
+function merge(to, from) {
+    for (n in from) {
+        if (typeof to[n] != 'object') {
+            to[n] = from[n];
+        } else if (typeof from[n] == 'object') {
+            to[n] = merge(to[n], from[n]);
+        }
+    }
+
+    return to;
+};
---
+++ tasks/options/clean.js
@@ -0,0 +1,3 @@
+﻿module.exports = {
+    all: ['./deploy/*', './build/*']
+}
