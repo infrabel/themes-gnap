43146afcc51ee7b09e0dfa3c16344f57d771d898
Infrabel <dotnet@infrabel.be>
2014-03-27 20:58:44 +0100

    Added jQuery 1.11.0

---
+++ tasks/jquery.js
@@ -0,0 +1,11 @@
+﻿module.exports = function (grunt) {
+    grunt.registerTask('build-jquery',
+    [
+        'deploy-jquery'
+    ]);
+    
+    grunt.registerTask('deploy-jquery',
+    [
+        'copy:raw_jquery_to_deploy'
+    ]);
+};
