602248dca6565fd6394348a3d867e02ec61fc8ca
David Cumps <david@cumps.be>
2014-04-10 09:41:42 +0200

    Prepare for taste and produce

---
+++ tasks/taste.js
@@ -0,0 +1,9 @@
+﻿module.exports = function(grunt) {
+    grunt.registerMultiTask('taste', 'test a theme', function () {
+        var t = this.target;
+
+        grunt.task.run('bake:' + t);
+
+        // TODO: Build nuget without incrementing version (or maybe put in a 0.0.0 version?)
+    });
+};
