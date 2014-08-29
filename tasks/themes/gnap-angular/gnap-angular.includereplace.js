4b6cfd8ce83aef15ce947bfe7040dcd1bbe4829c
David Cumps <david@cumps.be>
2014-08-29 15:25:24 +0200

    Doing includereplace for the code examples in Angular

---
+++ tasks/themes/gnap-angular/gnap-angular.includereplace.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    'gnap-angular_replace_examples': {
+        files: [
+            { src: '*.html', dest: './build/gnap-angular/', expand: true, cwd: './build/gnap-angular/' }
+        ]
+    }
+};
