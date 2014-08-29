073238a9b94da50175aef27df48f2971c37fca36
David Cumps <david@cumps.be>
2014-08-30 01:40:57 +0200

    Started adding examples to theme

--- custom/ace/html/js/index.js.replace-grunt.js
+++ custom/ace/html/js/index.js.replace-grunt.js
@@ -11,6 +11,10 @@
             {
                 from: /images : 'assets\/images'/g,
                 to: 'images : \'images\''
+            },
+            {
+                from: 'var output_file = \'output_folder/\'+page_name+\'.html\';',
+                to: 'result = result.replace(/<pre class="example">[\\s\\S]*?<code/g, \'<pre class="example"><code\');\n\t\t\tresult = result.replace(/<\\/code>[\\s\\S]*?<\\/pre>/g, \'</code></pre>\');\n\n\t\t\tvar output_file = \'output_folder/\'+page_name+\'.html\';',
             }
         ]
     }
