68b56f3b3723064d9a744b678cba1491cb19e312
Infrabel <dotnet@infrabel.be>
2014-08-29 10:56:16 +0200

    Started adding gnap-angular build

---
+++ tasks/themes/gnap-angular/gnap-angular.replace.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    'gnap-angular_map': {
+        src: ['./build/gnap-angular/js/debug/*.map', './build/gnap-angular/js/debug/**/*.map'],
+        overwrite: true,
+        replacements: [
+            {
+                from: '..\\\\develop\\\\',
+                to: 'develop\\\\'
+            }
+        ]
+    }
+};
