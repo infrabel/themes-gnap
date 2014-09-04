3b646f1cb360300528183b2581dd07bedcab583d
David Cumps <david@cumps.be>
2014-09-04 13:52:39 +0200

    Refactored GNaP Angular towards Papa Styleguide

---
+++ custom/gnap-angular/js/develop/gnap/breadcrumbs.service.js
@@ -0,0 +1,28 @@
+/**
+ * @desc service that keeps track of the current breadcrumbs
+ * @file breadcrumbs.service.js
+ */
+(function () {
+  angular
+    .module('gnap')
+    .factory('breadcrumbsService', breadcrumbsService);
+
+
+  function breadcrumbsService () {
+    var breadcrumbs = [];
+
+    return {
+      setBreadcrumbs: setBreadcrumbs,
+      getBreadcrumbs: getBreadcrumbs
+    };
+
+    function setBreadcrumbs(value) {
+      breadcrumbs = value;
+    };
+
+    function getBreadcrumbs() {
+      return breadcrumbs;
+    };
+  };
+
+})();
