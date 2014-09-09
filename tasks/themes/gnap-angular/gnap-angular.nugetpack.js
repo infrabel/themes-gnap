010001447958762e885d2a3e97e6677d8d1644cd
David Cumps <david@cumps.be>
2014-09-09 15:31:42 +0200

    Released GNaP.Angular 0.2

---
+++ tasks/themes/gnap-angular/gnap-angular.nugetpack.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    'gnap-angular': {
+        src: './nuget/definitions/GNaP.Web.Themes.GNaP.Angular.nuspec',
+        dest: './nuget/packages/GNaP.Themes.Web.GNaP.Angular',
+
+        options: {
+            basePath: './deploy/gnap-angular'
+        }
+    }
+};
