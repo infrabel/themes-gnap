5799977fa7e2985b483af39b9b6404d07d91ec90
Infrabel <dotnet@infrabel.be>
2014-09-09 15:31:42 +0200

    Released GNaP.Angular 0.2

---
+++ tasks/themes/gnap-angular/gnap-angular.semver.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    'gnap-angular': {
+        files: [
+            {
+                src: './versions/gnap-angular.json',
+                dest: './versions/gnap-angular.json'
+            }
+        ]
+    }
+};
