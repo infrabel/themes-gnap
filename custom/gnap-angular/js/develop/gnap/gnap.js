4fed48e4aa89201e8ec93715c6845e22fdccb69c
David Cumps <david@cumps.be>
2014-09-10 16:21:18 +0200

    Donated datepicker style - possibly for #17

--- custom/gnap-angular/js/develop/gnap/gnap.js
+++ custom/gnap-angular/js/develop/gnap/gnap.js
@@ -1,8 +1,16 @@
 angular
-    .module('gnap', ['ngAnimate', 'LocalStorageModule']);
+    .module('gnap', ['ngAnimate', 'LocalStorageModule', 'ui.bootstrap']);
 
 // configure the local storage service to use 'gnap' as the key prefix
-angular.module('gnap')
-    .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+angular
+    .module('gnap')
+    .config(['localStorageServiceProvider', function (localStorageServiceProvider) {
         localStorageServiceProvider.setPrefix('gnap');
+    }]);
+
+angular
+    .module('gnap')
+    .config(['datepickerConfig', function (datepickerConfig) {
+        datepickerConfig.showWeeks = false;
+        datepickerConfig.startingDay = '1';
     }]);
