5f03fccd246f011825aa1edac9102035aa16dee2
sroosen <s.roosen@gmail.com>
2014-09-11 15:38:14 +0200

    Adding ui-router (WIP)

---
+++ custom/gnap-angular/app/main/routes.js
@@ -0,0 +1,10 @@
+angular.module('gnap-app')
+    .config(['$stateProvider', function ($stateProvider) {
+
+        $stateProvider
+            .state('main', {
+                templateUrl: 'app/main/main.html',
+                controller: 'MainController'
+            });
+
+    }]);
