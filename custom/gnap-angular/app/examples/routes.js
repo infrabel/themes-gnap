0f74d39aabbc4cf96eb6eadec3ddb4ddb62472da
Infrabel <dotnet@infrabel.be>
2014-09-11 20:30:37 +0200

    Added iffys around all app scripts

--- custom/gnap-angular/app/examples/routes.js
+++ custom/gnap-angular/app/examples/routes.js
@@ -1,10 +1,12 @@
-angular.module('gnap-app')
-     .config(['$stateProvider', function ($stateProvider) {
-
-        $stateProvider
-            .state('main.examples', {
-                url: '/examples',
-                templateUrl: 'app/examples/examples.html',
-                controller: 'ExamplesController'
-            });
-    }]);
+(function () {
+    angular
+        .module('gnap-app')
+        .config(['$stateProvider', function ($stateProvider) {
+            $stateProvider
+                .state('main.examples', {
+                    url: '/examples',
+                    templateUrl: 'app/examples/examples.html',
+                    controller: 'ExamplesController'
+                });
+        }]);
+})();
