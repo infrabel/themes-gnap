8c07d8c09c635f21d3a0aef3d169fc22399d189e
David Cumps <david@cumps.be>
2014-09-12 13:30:27 +0200

    Fixed logo href

--- custom/gnap-angular/start-server.cmd
+++ custom/gnap-angular/start-server.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET PORT=40829
-ECHO Launching browser at http://localhost:%PORT%/examples.html - It is possibly you will have to refresh.
-explorer.exe http://localhost:%PORT%/index.html
+ECHO Launching browser at http://localhost:%PORT% - It is possibly you will have to refresh.
+explorer.exe http://localhost:%PORT%/
 
 ECHO Starting server at http://localhost:%PORT%/
 server\simple-http-server.exe %PORT% ..\
