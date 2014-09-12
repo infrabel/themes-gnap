78553bcbead55516f184dbead73e8ec71b4fd166
David Cumps <david@cumps.be>
2014-09-12 02:32:26 +0200

    Added gnap-highlight directive and updated examples

---
+++ custom/gnap-angular/js/develop/gnap/highlight.directive.js
@@ -0,0 +1,31 @@
+/**
+ * @desc applies code syntax coloring
+ * @file highlight.directive
+ * @example <code gnap-highlight class="html">html code here</code>
+ */
+(function () {
+    angular
+        .module('gnap')
+        .directive('gnapHighlight', gnapHighlight);
+
+    gnapHighlight.$inject = ['$timeout'];
+
+    function gnapHighlight($timeout) {
+
+        function link(scope, element, attrs) {
+            var language = attrs['gnapHighlight'];
+            var snippet = element.text();
+
+            $timeout(function () {
+                element.html((language)
+                    ? hljs.highlight(language, snippet).value
+                    : hljs.highlightAuto(snippet).value);
+            }, 0);
+        };
+
+        return {
+            restrict: 'A',
+            link: link
+        };
+    }
+})();
