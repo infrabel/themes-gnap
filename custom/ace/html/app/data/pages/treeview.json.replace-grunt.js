4138368874491b4a806b7bf4b1e8384ff6618ea0
David Cumps <david@cumps.be>
2014-09-14 20:28:57 +0200

    Added Treeview and Gallery + examples

---
+++ custom/ace/html/app/data/pages/treeview.json.replace-grunt.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    ace_html_app_data_pages_treeview: {
+        src: './build/ace/html/app/data/pages/treeview.json',
+        dest: './build/ace/html/app/data/pages/treeview.json',
+        replacements:
+        [
+            {
+                from: 'with selectable items(single & multiple) and custom icons',
+                to: ''
+            }
+        ]
+    }
+};
