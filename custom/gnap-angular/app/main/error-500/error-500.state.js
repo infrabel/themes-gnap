21c47456b89c90feb692f39aba7ea5e23402ea95
sroosen <s.roosen@gmail.com>
2014-09-15 15:41:14 +0200

    Moved child states of main under main directory

--- custom/gnap-angular/app/main/error-500/error-500.state.js
+++ custom/gnap-angular/app/main/error-500/error-500.state.js
@@ -4,16 +4,16 @@
         .config(stateConfig);
 
     var stateSettings = {
-        name: 'main.getting-started',
+        name: 'main.error-500',
         state: {
-            url: '/getting-started',
-            templateUrl: 'app/getting-started/getting-started.html',
-            controller: 'GettingStartedController'
+            url: '/error-500',
+            templateUrl: 'app/main/error-500/error-500.html',
+            controller: 'Error500Controller'
         },
         breadcrumb: {
-            title: 'Getting Started'
+            title: 'Error 500'
         },
-        sidebarKey: 'getting-started'
+        sidebarKey: 'error-500'
     };
 
     stateSettings.state.onEnter = onEnter;
