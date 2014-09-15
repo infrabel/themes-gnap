74ae2f24e27a035486dad2d01c895234423d3c80
sroosen <s.roosen@gmail.com>
2014-09-15 14:21:24 +0200

    Moved sidebar setup to state config

--- custom/gnap-angular/app/main/states/main.state.js
+++ custom/gnap-angular/app/main/states/main.state.js
@@ -25,15 +25,151 @@
         $stateProvider.state(stateSettings.name, stateSettings.state);
     };
 
-    onEnter.$inject = ['breadcrumbsService'];
+    onEnter.$inject = ['breadcrumbsService', 'sidebarService'];
 
-    function onEnter(breadcrumbsService) {
+    function onEnter(breadcrumbsService, sidebarService) {
+        setupSidebarShortcuts(sidebarService);
+        setupSidebarItems(sidebarService);
+
         breadcrumbsService.addBreadcrumb(stateSettings.breadcrumb);
     };
 
-    onExit.$inject = ['breadcrumbsService'];
+    onExit.$inject = ['breadcrumbsService', 'sidebarService'];
 
-    function onExit(breadcrumbsService) {
+    function onExit(breadcrumbsService, sidebarService) {
+        sidebarService.settings.shortcuts = [];
+        sidebarService.settings.items = [];
+
         breadcrumbsService.removeLastBreadcrumb();
+    };
+
+    function setupSidebarShortcuts(sidebarService) {
+        // configure shortcuts
+        sidebarService.settings.shortcuts = [
+            {
+                title: 'Statistics',
+                buttonClass: 'btn btn-success',
+                icon: 'icon-signal',
+                click: function () {
+                    alert('Going to the statistics page ...');
+                }
+            },
+            {
+                title: 'Edit',
+                buttonClass: 'btn btn-info',
+                icon: 'icon-pencil',
+                click: function () {
+                    alert('Going to the edit page ...');
+                }
+            },
+            {
+                title: 'Profile',
+                buttonClass: 'btn btn-warning',
+                icon: 'icon-group',
+                click: function () {
+                    alert('Going to the profile page ...');
+                }
+            },
+            {
+                title: 'Administration',
+                buttonClass: 'btn btn-danger',
+                icon: 'icon-cogs',
+                click: function () {
+                    alert('Going to the administration page ...');
+                }
+            }
+        ];
+    };
+
+    function setupSidebarItems(sidebarService) {
+        // configure menu items
+        sidebarService.settings.items = [
+            {
+                key: 'about',
+                title: 'About',
+                icon: 'icon-lightbulb',
+                state: 'main.about'
+            },
+            {
+                key: 'getting-started',
+                title: 'Getting Started',
+                icon: 'icon-fire',
+                state: 'main.getting-started'
+            },
+            {
+                key: 'examples',
+                title: 'Examples',
+                icon: 'icon-heart',
+                state: 'main.examples'
+            },
+            {
+                key: 'typography',
+                title: 'Typography',
+                icon: 'icon-text-width',
+                state: 'main.typography'
+            },
+            {
+                key: 'error-404',
+                title: 'Error 404',
+                icon: 'icon-circle',
+                state: 'main.error-404'
+            },
+            {
+                key: 'error-500',
+                title: 'Error 500',
+                icon: 'icon-circle-blank',
+                state: 'main.error-500'
+            },
+            {
+                key: 'login',
+                title: 'Login',
+                icon: 'icon-user',
+                click: function () {
+                    alert('Going to the Login page ...');
+                    sidebarService.setSelected('login');
+                }
+            },
+            {
+                key: 'multi-level-menu',
+                title: 'Multi Level Menu',
+                icon: 'icon-globe',
+                items: [
+                    {
+                        key: 'level-2',
+                        title: 'Level 2',
+                        icon: 'icon-leaf',
+                        click: function () {
+                            alert('Going to the level2 page ...');
+                            sidebarService.setSelected('multi-level-menu/level-2');
+                        }
+                    },
+                    {
+                        key: 'level-2-with-sub',
+                        title: 'Level 2 with Sub',
+                        icon: 'icon-pencil',
+                        items: [
+                            {
+                                key: 'level-3',
+                                title: 'Level 3',
+                                icon: 'icon-plus',
+                                click: function () {
+                                    alert('Going to the Level 3 page ...');
+                                    sidebarService.setSelected('multi-level-menu/level-2-with-sub/level-3');
+                                }
+                            },
+                            {
+                                key: 'next-level-3',
+                                title: 'Next Level 3',
+                                icon: 'icon-eye-open',
+                                click: function () {
+                                    alert('Going to the Next Level 3 page ...');
+                                    sidebarService.setSelected('multi-level-menu/level-2-with-sub/next-level-3');
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
     };
 })();
