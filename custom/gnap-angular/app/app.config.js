a652885c78bb702ec12d5a3fb41bf3c1bbcea248
sroosen <s.roosen@gmail.com>
2014-09-16 10:59:57 +0200

    #28 Provide Localisation Support

--- custom/gnap-angular/app/app.config.js
+++ custom/gnap-angular/app/app.config.js
@@ -11,5 +11,26 @@
         .config(['$urlRouterProvider', function ($urlRouterProvider) {
             $urlRouterProvider
                 .otherwise("/about");
+        }])
+
+        .config(['$translateProvider', function ($translateProvider) {
+            $translateProvider.useLoader('$translatePartialLoader', {
+                urlTemplate: '{part}/translations.{lang}.json',
+                loadFailureHandler: "partialLoaderErrorHandler"
+            });
+        }])
+
+        .config(['tmhDynamicLocaleProvider', function (tmhDynamicLocaleProvider) {
+            tmhDynamicLocaleProvider.localeLocationPattern('js/angular/i18n/angular-locale_{{locale}}.min.js');
+        }])
+
+        .run(['localeService', function (localeService) {
+            localeService.initialize(
+                [
+                    { name: 'nl', title: 'Nederlands', default: true },
+                    { name: 'fr', title: 'Français' },
+                    { name: 'en', title: 'English' }
+                ]);
         }]);
+
 })();
