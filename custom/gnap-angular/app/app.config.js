be519fc7e37c3d40616bb6ef445aa0fcea3bf957
David Cumps <david@cumps.be>
2014-09-16 12:57:16 +0200

    Cleaned up Localisation PR

--- custom/gnap-angular/app/app.config.js
+++ custom/gnap-angular/app/app.config.js
@@ -1,36 +1,52 @@
 (function () {
     angular
         .module('gnap-example-app')
+        .config(tooltipConfiguration)
+        .config(urlRouterConfiguration)
+        .config(translationConfiguration)
+        .config(localeConfiguration)
+        .run(localeInitalization);
 
-        .config(['$tooltipProvider', function ($tooltipProvider) {
-            $tooltipProvider.options({
-                appendToBody: true
-            });
-        }])
+    var defaultPage = '/about';
 
-        .config(['$urlRouterProvider', function ($urlRouterProvider) {
-            $urlRouterProvider
-                .otherwise("/about");
-        }])
+    var supportedLanguages = [
+        { name: 'nl', title: 'Nederlands', default: true },
+        { name: 'fr', title: 'Français' },
+        { name: 'en', title: 'English' }
+    ];
 
-        .config(['$translateProvider', function ($translateProvider) {
-            $translateProvider.useLoader('$translatePartialLoader', {
-                urlTemplate: '{part}/translations.{lang}.json',
-                loadFailureHandler: "partialLoaderErrorHandler"
-            });
-        }])
+    tooltipConfiguration.$inject = ['$tooltipProvider'];
 
-        .config(['tmhDynamicLocaleProvider', function (tmhDynamicLocaleProvider) {
-            tmhDynamicLocaleProvider.localeLocationPattern('js/angular/i18n/angular-locale_{{locale}}.min.js');
-        }])
+    function tooltipConfiguration($tooltipProvider) {
+        $tooltipProvider.options({
+            appendToBody: true
+        });
+    };
 
-        .run(['localeService', function (localeService) {
-            localeService.initialize(
-                [
-                    { name: 'nl', title: 'Nederlands', default: true },
-                    { name: 'fr', title: 'Français' },
-                    { name: 'en', title: 'English' }
-                ]);
-        }]);
+    urlRouterConfiguration.$inject = ['$urlRouterProvider'];
 
+    function urlRouterConfiguration($urlRouterProvider) {
+        $urlRouterProvider.otherwise(defaultPage);
+    };
+
+    translationConfiguration.$inject = ['$translateProvider'];
+
+    function translationConfiguration($translateProvider) {
+        $translateProvider.useLoader('$translatePartialLoader', {
+            urlTemplate: '{part}/translations.{lang}.json', // if not local, e.g.: https://server/translations/{lang}/{part}
+            loadFailureHandler: 'partialLoaderErrorHandler'
+        });
+    };
+
+    localeConfiguration.$inject = ['tmhDynamicLocaleProvider'];
+
+    function localeConfiguration(tmhDynamicLocaleProvider) {
+        tmhDynamicLocaleProvider.localeLocationPattern('js/angular/i18n/angular-locale_{{locale}}.min.js');
+    };
+
+    localeInitalization.$inject = ['localeService'];
+
+    function localeInitalization(localeService) {
+        localeService.initialize(supportedLanguages);
+    };
 })();
