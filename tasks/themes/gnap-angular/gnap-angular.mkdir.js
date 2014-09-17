2348dc5a313d85a87332b9be38d4765830e0a753
Infrabel <dotnet@infrabel.be>
2014-09-17 15:25:46 +0200

    Added packaging of releases

--- tasks/themes/gnap-angular/gnap-angular.mkdir.js
+++ tasks/themes/gnap-angular/gnap-angular.mkdir.js
@@ -1,7 +1,11 @@
 ﻿module.exports = {
     'gnap-angular_nuget': {
         options: {
-            create: ['./nuget/packages/GNaP.Themes.Web.GNaP.Angular']
+            create: [
+                './nuget/packages/GNaP.Themes.Web.GNaP.Angular',
+                './releases/GNaP.Themes.Web.GNaP.Angular',
+                './deploy/vendor'
+            ]
         }
     }
 };
