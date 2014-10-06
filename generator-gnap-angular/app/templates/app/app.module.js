e68a21289569d1d86b777be362c2323dd890d660
David Cumps <david@cumps.be>
2014-10-06 14:18:35 +0200

    Started Yo generator for #30

---
+++ generator-gnap-angular/app/templates/app/app.module.js
@@ -0,0 +1,15 @@
+(function () {
+    angular
+        .module('<%= appName %>', 
+                ['gnap',
+                 'ui.bootstrap',
+                 'ui.router',
+                 'ngSanitize',
+                 'ngResource',
+                 'ngBootbox',
+                 'ngProgressLite',
+                 'ui.select2',
+                 'pascalprecht.translate',
+                 'tmh.dynamicLocale',
+                 'datatables']);
+})();
