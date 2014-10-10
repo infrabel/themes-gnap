5d0ca97c614d8a6d1a7054066ecaaf31a3d98562
Stijn Roosen <s.roosen@gmail.com>
2014-10-10 10:40:10 +0200

    Added secure state (wip) and modified server api

---
+++ custom/gnap-angular/app/main/secure/secure.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    angular
+        .module('gnap-example-app')
+        .controller('SecureController', SecureController);
+
+    SecureController.$inject = [];
+
+    function SecureController() {
+    };
+    
+})();
