96616010152869f655722874aa371dcb72b656fd
David Cumps <david@cumps.be>
2014-10-19 15:05:04 +0200

    Cleaned up style

--- custom/gnap-angular/js/develop/gnap/authentication.interceptor.js
+++ custom/gnap-angular/js/develop/gnap/authentication.interceptor.js
@@ -13,13 +13,13 @@
         return {
             request: function (config) {
                 var token = tokenManagerService.getToken();
-                
+
                 // put session token in authorization header
                 if (token) {
                     config.headers = config.headers || {};
                     config.headers.Authorization = 'Bearer ' + token;
                 }
-                
+
                 return config;
             }
         };
