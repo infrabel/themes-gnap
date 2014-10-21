2fe348da6a361957c8df70add0856c4114db4376
David Cumps <david@cumps.be>
2014-10-21 14:09:38 +0200

    Added Base64 polyfill, fixes #83

---
+++ tasks/base64.js
@@ -0,0 +1,11 @@
+﻿module.exports = function (grunt) {
+    grunt.registerTask('build-base64',
+    [
+        'deploy-base64'
+    ]);
+    
+    grunt.registerTask('deploy-base64',
+    [
+        'copy:raw_base64_to_deploy'
+    ]);
+};
