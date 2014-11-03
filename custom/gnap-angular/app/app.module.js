5d8253d6b3309fdeb4c83b230889337bcf1b259e
David Cumps <david@cumps.be>
2014-11-03 13:10:07 +0100

    Cleaned up exception handling

--- custom/gnap-angular/app/app.module.js
+++ custom/gnap-angular/app/app.module.js
@@ -3,14 +3,6 @@
 (function () {
     angular
         .module('gnap-example-app', ['gnap',
-                                     'ui.bootstrap',
-                                     'ui.router',
                                      'ngSanitize',
-                                     'ngResource',
-                                     'ngBootbox',
-                                     'ngProgressLite',
-                                     'ui.select2',
-                                     'pascalprecht.translate',
-                                     'tmh.dynamicLocale',
-                                     'datatables']);
+                                     'ngResource']);
 })();
