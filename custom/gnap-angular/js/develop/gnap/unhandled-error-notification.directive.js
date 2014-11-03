457a05db8ec0144cf8e1362ce3b795bed28c3c0b
Stijn Roosen <s.roosen@gmail.com>
2014-11-03 11:11:37 +0100

    IE 8 support

--- custom/gnap-angular/js/develop/gnap/unhandled-error-notification.directive.js
+++ custom/gnap-angular/js/develop/gnap/unhandled-error-notification.directive.js
@@ -7,23 +7,27 @@
         .module('gnap')
         .directive('gnapUnhandledErrorNotification', gnapUnhandledErrorNotification);
 
-    gnapUnhandledErrorNotification.$inject = ['$translate', '$translatePartialLoader', 'unhandledErrorChannel', 'notification'];
+    gnapUnhandledErrorNotification.$inject = ['$window', '$translate', '$translatePartialLoader', 'unhandledErrorChannel', 'notification'];
 
-    function gnapUnhandledErrorNotification($translate, $translatePartialLoader, unhandledErrorChannel, notification) {
+    function gnapUnhandledErrorNotification($window, $translate, $translatePartialLoader, unhandledErrorChannel, notification) {
 
         function link(scope) {
             unhandledErrorChannel.onErrorOccurred(scope, onErrorOccurred);
         }
 
         function onErrorOccurred(error) {
-            $translate(['gnap.error-notification-title', 'gnap.error-notification-text']).then(function (translations) {
-                console.log(translations);
-                notification.show({
-                    type: 'error',
-                    title: translations['gnap.error-notification-title'],
-                    text: translations['gnap.error-notification-text']
+            try {
+                $translate(['gnap.error-notification-title', 'gnap.error-notification-text']).then(function (translations) {
+                    notification.show({
+                        type: 'error',
+                        title: translations['gnap.error-notification-title'],
+                        text: translations['gnap.error-notification-text']
+                    });
                 });
-            });
+            }
+            catch (e) {
+                $window.alert('An unexpected error has occurred.');
+            }
         }
 
         return {
