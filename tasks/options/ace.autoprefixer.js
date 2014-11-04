7bdc3b1d8aa9434660bc867a3df02b525d0fbf72
Infrabel <dotnet@infrabel.be>
2014-11-04 15:25:48 +0100

    Autoprefix GNaP CSS

---
+++ tasks/options/ace.autoprefixer.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    build_ace_css: {
+        options: {
+            browsers: ['> 0%', 'last 2 versions', 'ie 8', 'ie 9', 'Firefox ESR', 'Opera 12.1']
+        },
+        files: [{
+            expand: true,
+            cwd: './build/ace/css/develop/',
+            src: '**/*.css',
+            dest: './build/ace/css/develop/'
+        }]
+    }
+};
