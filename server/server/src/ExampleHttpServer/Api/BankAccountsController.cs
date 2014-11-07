41c0c25df51f258970fd8102189507e19e865905
sroosen <s.roosen@gmail.com>
2014-11-07 08:36:24 +0100

    Removed server packages

---
+++ server/server/src/ExampleHttpServer/Api/BankAccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Http;
+
+namespace ExampleHttpServer.Api
+{
+    public class BankAccountsController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<dynamic> GetAll()
+        {
+            return JsonConvert.DeserializeObject<IEnumerable<dynamic>>(File.ReadAllText(@"Api\BankAccounts.json"));
+        }
+    }
+}
