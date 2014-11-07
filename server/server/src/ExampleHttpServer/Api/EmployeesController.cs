728aa3fca6647dafd5a3b2ebb2a6bc263ccfb5bc
sroosen <s.roosen@gmail.com>
2014-11-07 08:36:24 +0100

    Removed server packages

---
+++ server/server/src/ExampleHttpServer/Api/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace ExampleHttpServer.Api
+{
+    public class EmployeesController : ApiController
+    {
+        [Authorize]
+        public IHttpActionResult Get()
+        {
+            return StatusCode(HttpStatusCode.Forbidden);
+        }
+    }
+}
