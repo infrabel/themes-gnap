088cc7709117e5998ea278e9080999d3bae795ce
Stijn Roosen <s.roosen@gmail.com>
2014-11-14 15:17:44 +0100

    Remote validation samle

--- custom/gnap-angular/js/develop/gnap/show-validity.directive.js
+++ custom/gnap-angular/js/develop/gnap/show-validity.directive.js
@@ -1,28 +1,28 @@
 ﻿'use strict';
 
 /**
- * @desc Directive that wraps elements in a Bootstrap 3 form group while adding validation class handling.
- * @file form-group.directive.js
- * @example <div gnap-form-group>f</div>
+ * @desc Directive that adds the 'has-error' class if the associated input field is invalid.
+ * @file show-validity.directive.js
+ * @example <div gnap-show-validity for="firstName"></div>
  */
 (function () {
     angular
         .module('gnap')
-        .directive('showValidityFor', showValidityFor);
+        .directive('gnapShowValidity', gnapShowValidity);
 
-    function showValidityFor() {
+    function gnapShowValidity() {
 
         function link($scope, element, attrs, ctrl) {
             var form = ctrl; // controller is form
 
             // watch for invalid state changes
-            $scope.$watch(function () { return form[attrs.showValidityFor].$invalid; }, updateHasErrorClass);
+            $scope.$watch(function () { return form[attrs.for].$invalid; }, updateHasErrorClass);
 
             // watch for the submit status to change
             $scope.$watch(function () { return form.$submitted; }, updateHasErrorClass);
 
             function updateHasErrorClass() {
-                if (form[attrs.showValidityFor].$invalid && (form[attrs.showValidityFor].$dirty || form.$submitted)) {
+                if (form[attrs.for].$invalid && (form[attrs.for].$dirty || form.$submitted)) {
                     element.addClass('has-error');
                 } else {
                     element.removeClass('has-error');
