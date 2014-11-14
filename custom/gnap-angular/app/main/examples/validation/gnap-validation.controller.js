a56ba5a7e5e4b1ce4417e7e2494b984a2bba087e
Stijn Roosen <s.roosen@gmail.com>
2014-11-14 07:51:28 +0100

    Validation (wip)

---
+++ custom/gnap-angular/app/main/examples/validation/gnap-validation.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+    angular
+        .module('gnap-example-app')
+        .controller('gnap-validation', GnapValidationController);
+
+    GnapValidationController.$inject = [];
+
+    function GnapValidationController() {
+        var vm = this;
+    }
+})();
