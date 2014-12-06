a9b566744c735486e0323ffb0c969f1d46737e14
David Cumps <david@cumps.be>
2014-12-06 01:53:39 +0100

    Nuget packages follow correct naming

--- tasks/themes/gnap/gnap.nugetpack.js
+++ tasks/themes/gnap/gnap.nugetpack.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
     gnap: {
         src: './nuget/GNaP.Web.Themes.GNaP.nuspec',
-        dest: './nuget/packages/GNaP.Themes.Web.GNaP',
+        dest: './nuget/GNaP.Themes.Web.GNaP',
 
         options: {
             basePath: './deploy/vendor/gnap-theme-gnap/'
