7054567dfcffff666295c1d7f210afab80788b47
Infrabel <dotnet@infrabel.be>
2014-12-06 01:53:39 +0100

    Nuget packages follow correct naming

--- tasks/themes/gnap-angular/gnap-angular.nugetpack.js
+++ tasks/themes/gnap-angular/gnap-angular.nugetpack.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
     'gnap-angular': {
         src: './nuget/GNaP.Web.Themes.GNaP.Angular.nuspec',
-        dest: './nuget/packages/GNaP.Themes.Web.GNaP.Angular',
+        dest: './nuget/GNaP.Themes.Web.GNaP.Angular',
 
         options: {
             basePath: './deploy/vendor/gnap-theme-gnap-angular/'
