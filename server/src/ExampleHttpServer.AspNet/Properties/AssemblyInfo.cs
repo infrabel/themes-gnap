4c7f931a8a328d243d62b565b79eba3af6e8b458
Stijn Roosen <s.roosen@gmail.com>
2015-01-07 17:19:44 +0100

    Server is now split into ASP.NET and self host

--- server/src/ExampleHttpServer.AspNet/Properties/AssemblyInfo.cs
+++ server/src/ExampleHttpServer.AspNet/Properties/AssemblyInfo.cs
@@ -1,36 +1,38 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExampleHttpServer")]
+using ExampleHttpServer.Core.Bootstrap;
+using Microsoft.Owin;
+
+[assembly: AssemblyTitle("ExampleHttpServer.AspNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Infrabel")]
-[assembly: AssemblyProduct("ExampleHttpServer")]
-[assembly: AssemblyCopyright("Copyright © Infrabel 2014")]
+[assembly: AssemblyProduct("ExampleHttpServer.AspNet")]
+[assembly: AssemblyCopyright("Copyright © Infrabel 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("56c0a9d6-6f15-403d-84ad-9c8db51871d0")]
+[assembly: Guid("b1f235c1-0de3-4d15-b40d-9067a2597325")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: OwinStartup(typeof(Startup))]
